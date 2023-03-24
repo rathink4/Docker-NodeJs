cddb1ac50033f6f4a3a500d7008830a6c9fa0be5
rathink4 <rathink4@gmail.com>
2023-03-24 12:40:39 -0500

    Added updates

--- middleware/usersMiddleware.js
+++ middleware/usersMiddleware.js
@@ -1,3 +1,6 @@
+// Makes it such that only user's which are signed up and logged in can do tasks
+// Used in routers/blogPostRoutes.js
+
 const protect = (req, res, next) => {
     const {user} = req.session
 
